1a3e310d58549ca3c3df5bcdbc3e629ac2efa8b8
MarichkaDenys <99760032+MarichkaDenys@users.noreply.github.com>
2022-12-01 17:41:18 +0200

    Add files via upload

---
+++ MagazineSerializer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Magazine.h"
+
+
+class MagazineSerializer
+{
+public:
+	static void ReadMagazinesFromFile(Magazine**& magazines, int& count);
+	static void WriteMagazinesToFile(Magazine** magazines, int count);
+
+	static void ReadMagazinesFromFile(Magazine*& magazines, int& count);
+	static void WriteMagazinesToFile(Magazine* magazines, int count);
+};
